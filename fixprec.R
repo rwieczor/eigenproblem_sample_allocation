83f20f03bd8a5464331d0205954e391e3b32cdec
rwieczor <rwieczor@gmail.com>
2016-12-28 12:40:03 +0100

    Update fixprec.R

--- fixprec.R
+++ fixprec.R
@@ -1,11 +1,12 @@
 #
 # This file contains R functions implementing the algorithms described  
-# in the paper: Wesolowski J., Wieczorkowski R.:
-#   "An eigenproblem approach to optimal equal-precision sample allocation 
-#    in subpopulations" 
-# (submitted to Communications in Statistics - Theory and Methods)
+# in the paper: Jacek Wesołowski & Robert Wieczorkowski (2017) An eigenproblem
+# approach to optimal equal-precision sample allocation in subpopulations, 
+# Communications in Statistics - Theory and Methods, 46:5, 2212-2231, 
+# DOI: 10.1080/03610926.2015.1040501
+# To link to this article: http://dx.doi.org/10.1080/03610926.2015.1040501
 #
-# actualization date: 26.04.2015
+# actualization date: 28.12.2016
 #
 
 
