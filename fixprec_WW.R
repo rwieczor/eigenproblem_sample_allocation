595348b4919e3b37e52dcdfa23ea21b90e565311
Wojtek <wojciech.wojciak@gmail.com>
2024-03-05 21:26:42 +0100

    Create fixprec_WW.R
    Added prototype of Recursive FIXPREC.

---
+++ fixprec_WW.R
@@ -0,0 +1,241 @@
+#' Function for equal-precision optimal allocation in single-stage sampling
+#' with sub-populations and strata, see Theorem 2.3 in the article
+#'
+#' @param n total sample size
+#' @param J vector with sub-populations labels
+#' @param N population sizes
+#' @param S population standard deviations of surveyed variable
+#' @param total totals of surveyed variable in sub-populations
+#' @param kappa priority weights for sub-populations
+#' @param active user supplied take-max strata set, indices of J.
+#' @param short short output
+#'
+#' @examples
+#' J <- c(1, 1, 2, 2) # two sub-populations with 2 strata each.
+#' N <- c(140, 110, 135, 190)
+#' S <- sqrt(c(180, 20, 5, 4))
+#' total <- c(2, 3)
+#' kappa <- c(0.4, 0.6)
+#' (n <- nmax(J, N, S) - 1)
+#'
+#' (x <- fixprec(n, J, N, S, total, kappa))
+#' x$n_ih # 162.47371 42.55264 142.98313 179.99052
+#' x$check_cnstr
+#' x$T_lambda # 0.8476689
+#'
+#' (x_opt <- fixprec(n, J, N, S, total, kappa, active = 1))
+#' x_opt$n_ih # 140 106.8521 124.4665 156.6814
+#' x_opt$check_cnstr
+#' x_opt$T_lambda # 40.50748
+#'
+fixprec <- function(n, J, N, S, total, kappa = NULL, active = NULL, details = TRUE) {
+  if (is.null(kappa)) {
+    kappa <- rep(1 / length(unique(J)), length(unique(J)))
+  }
+  rho <- total * sqrt(kappa)
+  rho2 <- total^2 * kappa
+  J0 <- J
+  N0 <- N
+  S0 <- S
+  n0 <- n
+
+  if (!is.null(active)) {
+    n <- n - sum(N[active])
+    J <- J[-active]
+    N <- N[-active]
+    S <- S[-active]
+  }
+
+  a <- as.matrix(tapply(N * S, J, sum) / rho)
+  c_ <- tapply(N * S^2, J, sum) / rho2 # - b (b = 0 if M = N)
+  D.matrix <- (a %*% t(a)) / n - diag(c_, nrow = length(c_))
+
+  eigenout <- eigen(D.matrix, symmetric = TRUE)
+  lambda <- eigenout$values[1] # largest eigenvalue
+  v <- eigenout$vectors[, 1] # corresponding eigenvector
+  if (lambda <= 0) {
+    stop("Largest eigenvalue is not strictly positive - solution does not exist!")
+  }
+  if (any(diff(sign(v)) != 0)) {
+    stop("eigenvector containts entries of a different sign")
+  }
+
+  s_i <- n * v / as.numeric(t(a) %*% v)
+  A <- (N * S) / rep(rho, times = table(J)) # brakets due to finite-prec arithmetic!
+  x <- rep(s_i, table(J)) * A
+  if (!is.null(active)) {
+    N <- N0
+    N[-active] <- x
+    x <- N
+  }
+
+  if (details) {
+    check_cnstr <- check_kkt(x, J0, N0, S0, total, kappa, n0, active, s_i, details = TRUE)
+    A <- (N0 * S0) / rep(rho, times = table(J0))
+    list(
+      T_lambda = lambda, Ti = kappa * lambda, check_cnstr = check_cnstr,
+      D.matrix = D.matrix, eigen = eigenout, N_over_A = N0 / A, s_i = s_i, n_ih = x
+    )
+  } else {
+    list(T_lambda = lambda, n_ih = x)
+  }
+}
+
+#' Check KKT conditions for FIXPREC problem.
+#'
+#' @return If `details` is `FALSE`, it returns optimal value of T
+#'  (if all constraints are satisfied) or `NA` (if any of the constraints is not satisfied).
+#'  If `details` is `TRUE`, a detailed constraints check is returned.
+#'
+#' @examples
+#' J <- c(1, 1, 2, 2) # two sub-populations with 2 strata each.
+#' N <- c(140, 110, 135, 190)
+#' S <- sqrt(c(180, 20, 5, 4))
+#' total <- c(2, 3)
+#' kappa <- c(0.4, 0.6)
+#' (n <- nmax(J, N, S) - 1)
+#'
+#' (x <- fixprec(n, J, N, S, total, kappa))
+#' check_kkt(x$n_ih, J, N, S, total, kappa, n, active = NULL, s = x$s_i, details = FALSE)
+#' check_kkt(x$n_ih, J, N, S, total, kappa, n, active = NULL, s = x$s_i, details = TRUE)
+check_kkt <- function(n_opt, J, N, S, total, kappa, n, active, s, tol = 10^-9, details = FALSE) {
+  Ti_notscaled <- tapply(N^2 / n_opt * S^2 - N * S^2, J, sum)
+  Ti <- Ti_notscaled / total^2
+  T_opt <- sum(Ti_notscaled) / sum(total^2 * kappa)
+
+  is_Ti_eq_kappa_T <- Ti - kappa * T_opt < tol
+  is_sum_nopt_eq_n <- sum(n_opt) - n < tol
+  n_ih_leq_N <- n_opt <= N
+
+  # check if mu_ih is non-negative for active constraints
+  is_mu_ih_nonneg <- if (is.null(active)) {
+    TRUE
+  } else {
+    rho <- total * sqrt(kappa)
+    A <- (N * S) / rep(rho, times = table(J))
+    s <- rep(s, times = table(J))
+    all((s >= N / A)[active])
+  }
+
+  if (details) {
+    list(
+      T_opt = T_opt,
+      Ti = Ti,
+      is_Ti_eq_kappa_T = is_Ti_eq_kappa_T,
+      is_sum_nopt_eq_n = is_sum_nopt_eq_n,
+      n_ih_leq_N = n_ih_leq_N,
+      is_mu_ih_nonneg = is_mu_ih_nonneg
+    )
+  } else {
+    if (all(is_Ti_eq_kappa_T, is_sum_nopt_eq_n, n_ih_leq_N, is_mu_ih_nonneg)) {
+      T_opt
+    } else {
+      NA_real_
+    }
+  }
+}
+
+#' Recursive FIXPREC algorithm (for 2 domains only)
+#'
+#' @examples
+#' J <- c(1, 1, 2, 2) # two sub-populations with 2 strata each.
+#' N <- c(140, 110, 135, 190)
+#' S <- sqrt(c(180, 20, 5, 4))
+#' total <- c(2, 3)
+#' kappa <- c(0.4, 0.6)
+#' (n <- nmax(J, N, S) - 1)
+#'
+#' (x <- rfixprec(n, J, N, S, total, kappa))
+#' x$x # 140 106.8521 124.4665 156.6814
+#' x$s_i # T = 0.8476689
+#'
+rfixprec <- function(n, J, N, S, total, kappa = NULL, domain = 1L) {
+  W <- seq(
+    from = sum(table(J)[0:(domain - 1)]) + 1,
+    length.out = table(J)[domain]
+  ) # all strata in a given domain
+
+  domain2 <- unique(J)[unique(J) != domain] # zalozenie: sa tylko dwie domeny
+  W2 <- seq(
+    from = sum(table(J)[0:(domain2 - 1)]) + 1,
+    length.out = table(J)[domain2]
+  ) # all strata in another domain
+  check_for_active2 <- W2
+
+  active <- NULL
+  active_temp <- NULL
+  iter <- 1
+  s_i <- NULL
+  repeat{
+    # Domena wyrozniona (domyslnie 1)
+    check_for_active <- W # potentially active strata in a given domain
+    active_temp <- active
+    repeat {
+      x <- fixprec(n, J, N, S, total, kappa, active_temp)
+      s_i <- rbind(s_i, c(iter, x$check_cnstr$T_opt, x$s_i)) # for debug only
+      x <- x$n_ih
+      active1 <- which(x[check_for_active] >= N[check_for_active])
+      if (length(active1) == 0L) {
+        break
+      } else {
+        active_temp <- c(active_temp, check_for_active[active1])
+        if (setequal(active1, W)) {
+          stop("Wszystkie wiezy na N aktywne w wyroznionej domenie") # wstepne symulacje pokazuje, ze niemozliwe
+        }
+        check_for_active <- check_for_active[-active1]
+      }
+    }
+    # Pozostala domena
+    active2 <- which(x[check_for_active2] >= N[check_for_active2])
+    if (length(active2) == 0L) {
+      break
+    } else {
+      active <- c(active, check_for_active2[active2])
+      if (setequal(active2, W2)) {
+        stop("Wszystkie wiezy na N aktywne w niewyroznionej domenie") # wstepne symulacje pokazuje, ze niemozliwe
+      }
+      check_for_active2 <- check_for_active2[-active2]
+      iter <- iter + 1
+    }
+  }
+
+  colnames(s_i) <- c("iter", "T", paste0("s_", 1:(ncol(s_i) - 2)))
+  list(n_ih = x, s_i = s_i, active = active_temp)
+}
+
+# Maximum allowed total sample size (see (16) from JW 2019), usually less than sum(N).
+nmax <- function(J, N, S) {
+  n_bound <- sum(tapply(N * S, J, sum)^2 / tapply(N * S^2, J, sum))
+  n_bound_floor <- floor(n_bound)
+  ifelse(n_bound == n_bound_floor, n_bound - 1, n_bound_floor)
+}
+
+# List with all possible subsets of J,
+# without subsets that are equal to entire domain (for any domain).
+subsets <- function(J) {
+  J <- J[J %in% names(table(J)[table(J) != 1L])] # remove domains with 1 stratum
+  subsets_i <- tapply(seq_along(J), J, function(i) {
+    m_i <- seq(1, length(i) - 1)
+    do.call(c, lapply(m_i, function(m_ii) combn(i, m_ii, simplify = FALSE)))
+  })
+  subsets_i_len <- sapply(subsets_i, length)
+  subsets_i_indices <- lapply(seq_along(subsets_i), function(i) {
+    seq(
+      from = ifelse(i == 1L, 1, subsets_i_len[i - 1] + 1),
+      length.out = subsets_i_len[i]
+    )
+  })
+  subsets_grid <- do.call(expand.grid, subsets_i_indices) # combination between domains
+  subsets_grid <- as.matrix(subsets_grid)
+  subsets_i <- do.call(c, subsets_i)
+  all_subsets <- c(
+    list(NULL),
+    subsets_i, # separate in domains
+    lapply(1:nrow(subsets_grid), function(r) setNames(unlist(subsets_i[subsets_grid[r, ]]), NULL)) # between domains
+  )
+  names(all_subsets) <- NULL
+  if (length(all_subsets) != sum(1, 2^table(J) - 2, prod(2^table(J) - 2))) {
+    stop("Zle obliczone podzbiory")
+  }
+  all_subsets
+}
